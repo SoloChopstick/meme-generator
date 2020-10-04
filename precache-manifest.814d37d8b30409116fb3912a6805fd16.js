bc6c3abf539ca8ee4f67688a76ccb77f9742cf51
Han Wen Xie <hanw.xie@gmail.com>
2020-10-04 14:54:09 -0400

    Updates

---
+++ precache-manifest.814d37d8b30409116fb3912a6805fd16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23c59fdeedb275f3d1afc1f632d486d",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "041399c8f15fae13c9cf",
+    "url": "/meme-generator/static/css/main.535828eb.chunk.css"
+  },
+  {
+    "revision": "8b0bb354c7f08c47e655",
+    "url": "/meme-generator/static/js/2.4bca04ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.4bca04ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041399c8f15fae13c9cf",
+    "url": "/meme-generator/static/js/main.f1115c4f.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
